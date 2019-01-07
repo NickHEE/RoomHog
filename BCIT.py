15346178c5a9678f7f78428ab2ab6b0c4fbb5987
NickHEE <35848933+NickHEE@users.noreply.github.com>
2019-01-06 18:50:20 -0800

    Add files via upload

---
+++ BCIT.py
@@ -0,0 +1,120 @@
+import requests
+import json
+import re
+
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+
+rooms = {
+    26: 'SW1-1104',
+    27: 'SW1-1105',
+    28: 'SW1-1106',
+    29: 'SW1-2110',
+    30: 'SW1-2111',
+    31: 'SW1-2112',
+    32: 'SW1-2113',
+    33: 'SW1-2186',
+    34: 'SW1-2187',
+    35: 'SW1-2513',
+    36: 'SW1-2515',
+    37: 'SW1-2517',
+    38: 'SW1-2519'
+}
+
+
+class BCITStudySession:
+    def __init__(self,
+                 urls,
+                 loginData,
+                 bookings,
+                 headers,
+                 **kwargs):
+        self.urls = urls
+        self.loginData = loginData
+        self.bookings = bookings
+        self.headers = headers
+        self.session = requests.Session()
+        self.login(**kwargs)
+
+    def login(self, **kwargs):
+        self.session.headers.update(self.headers)
+        res = self.session.post(self.urls['loginUrl'], data=self.loginData, **kwargs)
+
+        # Test login
+        if res.text.lower().find(self.loginData["NewUserName"].lower()) < 0:
+            raise Exception(f"could not log onto BCIT '{self.urls['loginUrl']}'"
+                            " (did not find successful login string)")
+        else:
+            print(f"\n***Login as {self.loginData['NewUserName']} successful!***\n")
+
+    def book(self):
+        page = self.session.get(self.urls['baseUrl']+f'day.php?year={self.bookings["start_year"]}&'
+                                                            f'month={self.bookings["start_month"]}&'
+                                                              f'day={self.bookings["start_day"]}&'
+                                                             f'area=4') # **Add multiple areas later
+        # **Change to use booking obj
+        s_hr = int(self.bookings["start_seconds"]) // 3600
+        s_m = int(self.bookings["start_seconds"]) % 3600 // 60
+        e_hr = int(self.bookings["end_seconds"]) // 3600
+        e_m = int(self.bookings["end_seconds"]) % 3600 // 60
+        length = abs((e_hr - s_hr) + ((e_m - s_m) / 60))
+
+        soup = BeautifulSoup(page.text, features='html.parser')
+        emptyEntries = soup.find_all('td', {'class': 'new'})
+        emptyStarts = [entry for entry in emptyEntries if f'hour={s_hr}&minute={s_m}' in entry.find('a')['href']]
+        availableRooms = []
+
+        for t in emptyStarts:
+            seq = t.find_next_siblings('td', {'class': 'new'}, limit=(length * 2) - 1)
+            if len(seq) == (length * 2 - 1):
+                room = re.search(r'room=(\d+)', t.find('a')['href']).group(1)
+                availableRooms.append(room)
+                # print(f"Room {rooms[int(room)]} is available!\n")
+
+        # **add room preference, check if room was booked?
+        if availableRooms:
+            print("Room Found!")
+            self.bookings['rooms[]'] = availableRooms[0]
+            self.bookings['create_by'] = self.loginData['NewUserName']
+            b = self.session.post('https://studyrooms.lib.bcit.ca/edit_entry_handler.php', data=self.bookings)
+            # print(b.text)
+            self.session.close()
+            return rooms[int(availableRooms[0])]
+        else:
+            self.session.close()
+            return False
+
+
+class Booking:
+    def __init__(self, date, length, cell, account=('', '')):
+        print(date)
+        self.length = length
+        self.account = account
+        self.startDate = datetime.strptime(date, '%I:%M %p %m/%d %Y')
+        self.endDate = self.startDate + timedelta(hours=int(length))
+        self.startSeconds = int(self.startDate.hour) * 3600
+        self.endSeconds = int(self.endDate.hour) * 3600
+        self.room = ''
+        self.cell = cell
+
+    def __repr__(self):
+        return(f'Date: {str(self.startDate.date())}\n'
+               f'Time: {str(self.startDate.hour)}:{str(self.startDate.minute)} to '
+               f'{str(self.endDate.hour)}:{str(self.endDate.minute)} \n'
+               f'Room: {self.room}\n')
+
+    def BookingToJson(self, bookTemplate):
+        bookTemplate['start_day'] = self.startDate.day
+        bookTemplate['start_month'] = self.startDate.month
+        bookTemplate['start_year'] = self.startDate.year
+        bookTemplate['start_seconds'] = self.startSeconds
+        bookTemplate['end_day'] = self.endDate.day
+        bookTemplate['end_month'] = self.endDate.month
+        bookTemplate['end_year'] = self.endDate.year
+        bookTemplate['end_seconds'] = self.endSeconds
+
+        return bookTemplate
+
+
+
---
+++ RoomHog.py
@@ -0,0 +1,76 @@
+import json
+import os
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+from BCIT import BCITStudySession, Booking
+
+# Cell References
+TIMES = 2
+DATES = 3
+
+script_dir = os.path.dirname(__file__)
+
+urls = {
+    'baseUrl': 'https://studyrooms.lib.bcit.ca/',
+    'loginUrl': 'https://studyrooms.lib.bcit.ca/admin.php',
+    'bookUrl': 'https://studyrooms.lib.bcit.ca/edit_entry_handler.php'
+}
+
+# Load JSONs
+with open(os.path.join(script_dir, r'json/headers.json')) as f:
+    headers = json.load(f)
+with open(os.path.join(script_dir, r'json/logins.json')) as f:
+    accounts = json.load(f)
+with open(os.path.join(script_dir, r'json/login_template.json')) as f:
+    login_template = json.load(f)
+with open(os.path.join(script_dir, r'json/book_template.json')) as f:
+    book_template = json.load(f)
+
+# Initialize gspread and get current sheet
+scope = [r'https://spreadsheets.google.com/feeds', r"https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(script_dir,'json/client_secret.json'), scope)
+client = gspread.authorize(creds)
+sheet = client.open('Room Hog').get_worksheet(1)
+schedule = sheet.range('C5:I29')
+
+# Parse the requests sheet and create a list of bookings
+bookings = [Booking(date=sheet.cell(c.row, TIMES).value + sheet.cell(DATES, c.col).value + f" {sheet.cell(2, 3).value}",
+                    length=c.value,
+                    cell=c)
+            for c in schedule if c.value]
+
+# Attempt to find and book a room for each request
+for booking in bookings:
+    for login in accounts['logins']:
+        if login['Active'] < 2:
+            break
+    else:
+        raise Exception("Not enough accounts available, too many active bookings.")
+
+    login_template['NewUserName'] = login['ID']
+    login_template['NewUserPassword'] = login['Password']
+    bcit = BCITStudySession(urls=urls,
+                            loginData=login_template,
+                            bookings=booking.BookingToJson(book_template),
+                            headers=headers)
+    room = bcit.book()
+    updateRange = sheet.range(booking.cell.row, booking.cell.col, booking.cell.row + (int(booking.length) * 2)-1,
+                              booking.cell.col)
+    if room:
+        login['Active'] = login['Active'] + 1
+        booking.room = room
+        for cell in updateRange:
+            cell.value = booking.room
+    else:
+        for cell in updateRange:
+            cell.value = "Not Available"
+    sheet.update_cells(updateRange)
+    print('\n***Booking Successful!***\n')
+    print(booking)
+
+
+
+
+
